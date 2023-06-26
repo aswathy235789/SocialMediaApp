5e9e4d127a250d29bfcacb58f3570dc4735c09b9
Aswathy Suresh(UST <235789@ust-global.com>
2023-06-26 20:48:11 +0530

    Posts entity created

--- src/main/java/com/mdemo/webservices/mdemo/User.java
+++ src/main/java/com/mdemo/webservices/mdemo/User.java
@@ -2,20 +2,39 @@ package com.mdemo.webservices.mdemo;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import org.springframework.cglib.core.Local;
 import java.time.LocalDate;
+import java.util.List;
 
+@Entity
+@Table(name="user_details")
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min = 2, message = "Name should have atleast 2 characters")
 //    @JsonProperty("User_name")
     private  String name;
     @Past(message = "Invalid Date of birth")
 //    @JsonProperty("Date_of_Birth")
-    @JsonIgnore
+//    @JsonIgnore
+    @Column(name="birth_date")
     private LocalDate birthDate;
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    @OneToMany(mappedBy ="user")
+    @JsonIgnore
+    private List<Post> posts;
 
     public Integer getId() {
         return id;
@@ -45,6 +64,9 @@ public class User {
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
+    }
+
+    public User() {
     }
 
     @Override
---
+++ src/main/java/com/mdemo/webservices/mdemo/UserJpaResource.java
@@ -0,0 +1,81 @@
+package com.mdemo.webservices.mdemo;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class UserJpaResource {
+
+    private UserDaoService service;
+    private PostRepository postRepository;
+    private UserRepository userRepository;
+    public UserJpaResource(UserDaoService service, UserRepository userRepository, PostRepository postRepository) {
+        this.service = service;
+        this.userRepository=userRepository;
+        this.postRepository=postRepository;
+    }
+
+    // GET /users
+    @GetMapping("/jpa/users")
+    public List<User> retrieveAllUsers() {
+        //return service.findAll();
+        return userRepository.findAll();
+    }
+
+    //     GET /users
+    @GetMapping("/jpa/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        //User user= (User) userRepository.findById(id).orElse(null);
+        User user= (User) userRepository.findById(id).orElse(null);
+        if(user==null)
+            throw new UserNotFoundException("Id :"+id);
+        return user;
+    }
+    @PostMapping("/jpa/users")
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user)
+    {
+        User savedUser=service.save(user);
+        URI location= ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/jpa/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/jpa/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+//        service.deleteById(id);
+
+      userRepository.deleteById(id);
+    }
+    @GetMapping("/jpa/users/{id}/posts")
+    public List<Post> retrievePostsForUser(@PathVariable int id) {
+
+
+        Optional<User> user=userRepository.findById(id);
+        if(user.isEmpty())
+            throw new UserNotFoundException("Id-"+id);
+        return user.get().getPosts();
+    }
+
+    @GetMapping("/jpa/users/{id}/posts")
+    public List<Post> createPostForUser(@PathVariable int id,@Valid @RequestBody Post post) {
+
+
+        Optional<User> user=userRepository.findById(id);
+        if(user.isEmpty())
+            throw new UserNotFoundException("Id-"+id);
+
+        post.setUser(user.get());
+        postRepository.save(post);
+        return user.get().getPosts();
+
+    }
+
+
+}
