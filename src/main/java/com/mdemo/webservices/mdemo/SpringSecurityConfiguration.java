ceeaef9279b23f4dd85a92748e47c1d0edfba90e
Aswathy Suresh(UST <235789@ust-global.com>
2023-07-02 11:38:55 +0530

    Spring security

---
+++ src/main/java/com/mdemo/webservices/mdemo/SpringSecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.mdemo.webservices.mdemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SpringSecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //1) All requests should be authenticated
+        http.authorizeHttpRequests(
+               auth -> auth.anyRequest().authenticated()
+        );
+        //2) If a request is not authenticated, a web page is shown
+
+        http.httpBasic(withDefaults());
+        //3) CSRF -> POST, PUT
+//         http.csrf().disable(); --> To disable csrf
+
+
+
+        return http.build();
+    }
+}
